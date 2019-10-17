94342dd7028ee2763e22e01803663c7b5b52b0ea
starkfree <starkfree@gmail.com>
2019-10-17 12:46:05 -0400

    Fixed Dependency Issues

--- core/config.py
+++ core/config.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.join( os.path.dirname( os.path.realpath( __file__ )), '..' )
 with open( BASE_DIR + "/config/config.yml", "r" ) as configFile :
     data = configFile.read( )
 
-data = yaml.load( data )
+data = yaml.load( data, Loader=yaml.FullLoader )
 
 # ACCESS KEY
 ACCESS_KEY = data['orcs']['access_key']
